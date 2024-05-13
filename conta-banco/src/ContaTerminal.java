b3be9933113f95ed32379fb388b4c9b6d0fa2e2f
Michel Braga <michel.diener@gmail.com>
2024-05-12 22:40:18 -0300

    exercise bank terminal - basic java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Random aleatorio = new Random();
+        int numero = aleatorio.nextInt(10000); // Generate a random number up to 9999
+        String numeroFormatado = String.format("%04d", numero); // Format the number with leading zeros
+        String nomeCliente;
+        String agenciaNumero;
+        Double saldoCliente;
+
+         
+        System.out.println("Desafio DIO -  Conta Banco");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o numero da agência: ");
+        agenciaNumero = scanner.nextLine();
+
+        System.out.print("Digite o saldo do cliente R$: ");
+        saldoCliente= (double) scanner.nextInt();
+
+        System.out.println("Olá "+ nomeCliente + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é " + 
+        agenciaNumero + ", conta " + numeroFormatado + " e seu saldo R$" + saldoCliente + 
+        " já está disponível para saque");
+
+        scanner.close();
+
+
     }
 }

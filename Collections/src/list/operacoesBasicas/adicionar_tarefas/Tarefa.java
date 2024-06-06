79cd8b29788790e33e2689ea2e6868897f5b748e
Michel Braga <michel.diener@gmail.com>
2024-06-05 22:03:58 -0300

    List interface

---
+++ Collections/src/list/operacoesBasicas/adicionar_tarefas/Tarefa.java
@@ -0,0 +1,19 @@
+package list.operacoesBasicas.adicionar_tarefas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return  descricao;
+    }
+}
